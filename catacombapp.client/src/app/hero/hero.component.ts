28e44320e373d371e10e866a5c7c62028f7a58ec
thejackbitt <jack.bittner@outlook.com>
2024-07-02 14:50:20 -0500

    starter page with base page

---
+++ catacombapp.client/src/app/hero/hero.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+
+public slides = [
+  { src }
+];
+
+@Component({
+  selector: 'app-hero',
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css'
+})
+
+export class HeroComponent {
+
+
+
+  constructor() {
+
+  }
+
+}
