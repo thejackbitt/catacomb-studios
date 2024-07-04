937dace4f4dcfaf4a12f02afc3acb53d45008357
thejackbitt <jack.bittner@outlook.com>
2024-07-03 19:59:57 -0500

    fixed homepage landing screen

--- CatacombApp.Server/obj/Debug/net8.0/CatacombApp.Server.AssemblyInfo.cs
+++ CatacombApp.Server/obj/Debug/net8.0/CatacombApp.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatacombApp.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28e44320e373d371e10e866a5c7c62028f7a58ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f24b2c3884001626e0b446c4f6f874e68ecf696a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatacombApp.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatacombApp.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
