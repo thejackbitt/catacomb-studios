2de18e81632288e11704c362f904370dc689b24c
thejackbitt <jack.bittner@outlook.com>
2024-07-03 21:00:53 -0500

    added info component

--- catacombapp.client/src/app/app.module.ts
+++ catacombapp.client/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroComponent } from './hero/hero.component';
 import { HeaderComponent } from './header/header.component';
+import { InfoComponent } from './info/info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroComponent,
-    HeaderComponent
+    HeaderComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
